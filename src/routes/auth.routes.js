c202baf2696ba83e39a72a82002b2435f22f2a4a
anugrahshinde23 <andyy5325@gmail.com>
2026-02-06 18:57:44 +0530

    removed middleware from logout route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
-router.post('/logout', verifyJWT, logout)
+router.post('/logout', logout)
 router.post('/generateNewAccessToken', generateNewAccessToken)
 router.get('/me', verifyJWT, getMe)
 
