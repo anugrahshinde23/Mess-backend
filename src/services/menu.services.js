24581282863b8ca7925bd9d2b1ebcef31b10f340
anugrahshinde23 <andyy5325@gmail.com>
2026-03-09 22:51:13 +0530

    added the rag pipeline in backend7

--- src/services/menu.services.js
+++ src/services/menu.services.js
@@ -2,34 +2,67 @@ import Menu from "../models/menu.model.js"
 import Mess from "../models/mess.model.js"
 import { getTodaysDay } from "../utility/getTodaysDay.js"
 
-export const createMenu = async(ownerId, menuData) => {
-    const mess = await Mess.findOne({
-        owner : ownerId
-    })
+// export const createMenu = async(ownerId, menuData) => {
+//     const mess = await Mess.findOne({
+//         owner : ownerId
+//     })
 
-    if(!mess){
-        throw new Error("Mess not found")
-    }
+//     if(!mess){
+//         throw new Error("Mess not found")
+//     }
 
-    const messId = mess._id
+//     const messId = mess._id
 
-    const today = getTodaysDay()
+//     const today = getTodaysDay()
 
-    const menu = await Menu.findOneAndUpdate({
-        mess : messId, day : today
-    },{
-        mess : messId,
-        day : today,
-        breakfast : menuData.breakfast,
-        lunch : menuData.lunch,
-        dinner : menuData.dinner
-    },{
-        new : true, upsert : true
-    })
+//     const menu = await Menu.findOneAndUpdate({
+//         mess : messId, day : today
+//     },{
+//         mess : messId,
+//         day : today,
+//         breakfast : menuData.breakfast,
+//         lunch : menuData.lunch,
+//         dinner : menuData.dinner
+//     },{
+//         new : true, upsert : true
+//     })
     
 
-    return menu
-}
+//     return menu
+// }
+
+export const createMenu = async(ownerId, menuData) => {
+    const mess = await Mess.findOne({ owner: ownerId });
+    if (!mess) throw new Error("Mess not found");
+
+    const messId = mess._id;
+    const today = getTodaysDay();
+
+    // 1. Pehle document ko find karo
+    let menu = await Menu.findOne({ mess: messId, day: today });
+
+    if (menu) {
+        // 2. Agar menu mil gaya, toh values update karo
+        menu.breakfast = menuData.breakfast;
+        menu.lunch = menuData.lunch;
+        menu.dinner = menuData.dinner;
+    } else {
+        // 3. Agar nahi mila, toh naya create karo
+        menu = new Menu({
+            mess: messId,
+            day: today,
+            breakfast: menuData.breakfast,
+            lunch: menuData.lunch,
+            dinner: menuData.dinner
+        });
+    }
+
+    
+    await menu.save();
+
+    return menu;
+};
+
 
 export const getTodaysMenu = async(ownerId) => {
     const mess = await Mess.findOne({
